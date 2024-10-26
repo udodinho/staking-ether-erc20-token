d186f4aa72d992ca22da7dd34972081ae3802e86
udodinho <udodinho200@gmail.com>
2024-10-26 15:02:56 +0100

    fix: add a check before withdrawal

--- contracts/UdvToken.sol
+++ contracts/UdvToken.sol
@@ -1,35 +1,51 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.24;
 
-contract StakingERCToken {
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+
+contract StakingERC20Token {
     struct Stake {
         uint256 stakingTime;
-        uint256 durationInSeconds;
+        uint256 durationInDays;
         uint256 amount;
         uint256 rewards;
         bool isStaked;
     }
     
     address owner;
-    mapping(address => Stake[]) stakes;
+    mapping (address => uint256) balances;
+    mapping (address => Stake[]) stakes;
+    address immutable TOKENADDRESS;
+    // 0x9a2E12340354d2532b4247da3704D2A5d73Bd189
 
-    constructor() {
+    constructor(address _tokenAddress) {
         owner = msg.sender;
+        TOKENADDRESS = _tokenAddress;
     }
 
     event StakedSuccessful(address user, uint256 amount);
     event WithdrawSuccessful(address user, uint256 _totalRewards);
 
-    function stake(uint256 _duration) external payable {
+    function stakeERCToken(uint256 _amount, uint256 _duration) external payable {
         require(msg.sender != address(0), "you can't stake with an address zero");
-        require(msg.value > 0, "staking 0 is not allowed");
+        require(_amount > 0, "staking 0 is not allowed");
+
+        uint256 _userBalance = IERC20(TOKENADDRESS).balanceOf(msg.sender);
+
+        require(_amount > _userBalance, "Insufficient balance");
+
+        IERC20(TOKENADDRESS).approve(address(this), _amount);
+        IERC20(TOKENADDRESS).transferFrom(msg.sender, address(this), _amount);
 
         uint256 stakingStart = block.timestamp;
 
+        balances[msg.sender] += _amount;
+
         Stake memory userStake = Stake({
             stakingTime: stakingStart,
-            durationInSeconds: _duration * 365 days,
-            amount: msg.value,
+            durationInDays: block.timestamp + (_duration * 1 days),
+            amount: _amount,
             rewards: 0,
             isStaked: true
         });
@@ -37,7 +53,7 @@ contract StakingERCToken {
         stakes[msg.sender].push(userStake);
     }
 
-    function withdrawRewards(uint256 _stakeId) external {
+    function withdrawRewards(uint256 _amount, uint256 _stakeId) external {
         require(msg.sender != address(0), "You can't withdraw with address zero");
         require(_stakeId < stakes[msg.sender].length, "Invalid stake ID");
 
@@ -46,7 +62,9 @@ contract StakingERCToken {
         
         uint256 _totalRewards;
 
-        if (block.timestamp > staker.stakingTime + staker.durationInSeconds) {
+        require(balances[msg.sender] < _amount, "Insufficient funds");
+
+        if (block.timestamp > staker.stakingTime + staker.durationInDays) {
             _totalRewards = staker.amount + calculateInterestRate(msg.sender, _stakeId);
         } else {
             _totalRewards = staker.amount;
@@ -67,10 +85,10 @@ contract StakingERCToken {
 
     function calculateInterestRate(address _user, uint256 _stakeId) private view returns (uint256) {
         Stake storage staker = stakes[_user][_stakeId];
-        uint256 duration = block.timestamp - staker.stakingTime;
-        uint256 interestRatePerYear = 5 * 1e18; // 5% interest per year
+        uint256 duration = block.timestamp - staker.durationInDays;
+        uint256 interestRatePerYear = 5 * 1e5; // 5% interest per year
 
-        uint256 interest = (staker.amount * interestRatePerYear * duration) / (365 days * 1e18);
+        uint256 interest = (staker.amount * interestRatePerYear * duration) / (365 days * 1e5);
         return interest;
     }
 
